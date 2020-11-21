b9d85ebdb653c23bd0ce35703f4bcd0748d57e62
TheBlipBloop <blipbloopdev@gmail.com>
2020-11-21 14:35:55 -0700

    Add more functionality to Vector2.java Add work in progress version of the travelToPoint function, this function has been implemented successfully in a simulated physics environment, but requires conversion to FTC Java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/DriveTrain.java
@@ -1,5 +1,45 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
+import org.firstinspires.ftc.teamcode.Helpers.Vector2;
+
 public class DriveTrain {
 
+    // WIP, not commented or formatted
+    public void travelToPoint(Vector2 endPoint, double minSpeed, double maxSpeed, double turnSpeed, double terminationDistance) {
+        //TODO - using PID, rotate quickly towards our end point. Does not need to be accurate, (within +- 60 degrees of our endPoint)
+
+        Vector2 directionToEndPoint = Vector2.zero;
+        double startingDistanceToEndPoint = 1000;
+        double currentDistanceToEndPoint = 1000;
+
+        double powerRight = 0;
+        double powerLeft = 0;
+
+        double powerCoefficient = 0;
+
+        startingDistanceToEndPoint = Vector2.magnitude(Vector2.subtract(getLocation(), endPoint));
+
+        while (currentDistanceToEndPoint * currentDistanceToEndPoint > terminationDistance) {
+            directionToEndPoint = Vector2.normalOf(Vector2.subtract(getLocation(), endPoint));
+            currentDistanceToEndPoint = Vector2.magnitude(Vector2.subtract(getLocation(), endPoint));
+
+            powerCoefficient = (currentDistanceToEndPoint / startingDistanceToEndPoint) * Math.PI;
+            powerCoefficient = Math.sin(powerCoefficient);
+
+            powerRight = (maxSpeed * powerCoefficient) + minSpeed;
+            powerLeft = (maxSpeed * powerCoefficient) + minSpeed;
+        }
+
+        //TODO - stopWheelMotors
+    }
+
+    // TODO - implement me!
+    public Vector2 getLocation() {
+        return Vector2.zero;
+    }
+
+    // TODO - implement me!
+    public double getRotation() {
+        return 0;
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helpers/Vector2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helpers/Vector2.java
@@ -4,19 +4,7 @@ import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 
 // Two dimensional double vector
 public class Vector2 {
-    protected double x;
-    protected double y;
-
-    // Creates the vector (0,0)
-    public Vector2() {
-        x = 0;
-        y = 0;
-    }
-
-    public Vector2(double xValue, double yValue) {
-        x = xValue;
-        y = yValue;
-    }
+    public static Vector2 zero = new Vector2(0, 0);
 
     // Adds the x and y values of vector A and B and returns the resulting vector
     public static Vector2 add(Vector2 a, Vector2 b) {
@@ -26,6 +14,26 @@ public class Vector2 {
     // Subtracts the x and y values of vector A and B and returns the resulting vector
     public static Vector2 subtract(Vector2 a, Vector2 b) {
         return new Vector2(a.x - b.x, a.y - b.y);
+    }
+
+    // Divides the x and y values of vector A and B and returns the resulting vector
+    public static Vector2 divide(Vector2 a, Vector2 b) {
+        return new Vector2(a.x / b.x, a.y / b.y);
+    }
+
+    // Divides the x and y values of vector A by double B and returns the resulting vector
+    public static Vector2 divide(Vector2 a, double b) {
+        return new Vector2(a.x / b, a.y / b);
+    }
+
+    public static Vector2 normalOf(Vector2 value) {
+        double magnitude = magnitude(value);
+
+        if (magnitude(value) > 0.0001) {
+            return divide(value, magnitude);
+        } else {
+            return zero;
+        }
     }
 
     // Returns the dot produce of a and b, between -1 and 1
@@ -45,7 +53,20 @@ public class Vector2 {
 
     // Returns a heading vector from a radian angle measure
     public static Vector2 fromRadians(double angle) {
-       return new Vector2(Math.cos(angle),Math.sin(angle));
+        return new Vector2(Math.cos(angle), Math.sin(angle));
     }
 
+    protected double x;
+    protected double y;
+
+    // Creates the vector (0,0)
+    public Vector2() {
+        x = 0;
+        y = 0;
+    }
+
+    public Vector2(double xValue, double yValue) {
+        x = xValue;
+        y = yValue;
+    }
 }
