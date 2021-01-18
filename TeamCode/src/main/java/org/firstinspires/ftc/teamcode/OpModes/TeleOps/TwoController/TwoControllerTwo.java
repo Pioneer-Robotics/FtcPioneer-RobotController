89d9613f1e60517977054eb9226bcc8c6b3ea635
gianluca-tenino <gianluca.tenino@gmail.com>
2021-01-18 14:07:31 -0700

    Rearranged Telemetry and added a function to set launch speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOps/TwoController/TwoControllerTwo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOps/TwoController/TwoControllerTwo.java
@@ -5,25 +5,29 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Hardware.Config;
 import org.firstinspires.ftc.teamcode.Hardware.Robot;
 import org.firstinspires.ftc.teamcode.Helpers.DataHub;
 import org.firstinspires.ftc.teamcode.Helpers.Toggle;
 import org.firstinspires.ftc.teamcode.OpModes.TeleOps.TeleOpScript;
 //comment
 public class TwoControllerTwo extends TeleOpScript {
+    private static float TRIGGER_DEADZONE = 0.1f;
+
     double drive, turn, tgtPowerLeft, tgtPowerRight, driveScale;
     ElapsedTime deltaTime;
+    ElapsedTime loopTime;
     Telemetry telemetry;
     Gamepad gamepad1;
     Gamepad gamepad2;
     Toggle moveStraightFast;
     Toggle collecting;
-    Toggle launcherOff;
-    Toggle spool;
-    Toggle continuousFire;
-    Toggle fire;
-    Toggle launchOverride;
     Toggle stopCollecting;
+    Toggle gamepad2LeftTrigger;
+    Toggle gamepad2RightTrigger;
+
+
+    double launcherSpeedFraction = 0.8;
 
 
 
@@ -32,23 +36,40 @@ public class TwoControllerTwo extends TeleOpScript {
         drive = gamepad1.left_stick_y;
         turn = gamepad1.right_stick_x;
 
-        launcherOff.toggle(gamepad1.x|| gamepad2.x);
-        if (launcherOff.justChanged()){
-            Robot.get().cancelLaunch();
-        }
+        /*============
+        Launcher Control
+         ============*/
+        //overrides the launch conditions to initiate the launch asap
+        Robot.get().launchOverride(gamepad2.right_bumper);
 
-        Robot.get().launchOverride(gamepad2.right_trigger != 0);
-
+        //spools without initiating a launch
         if (gamepad1.y){
             Robot.get().spool();
         }
+
+        //sets the launchmode to IDLE; should be used if there is an issue and a launch cannot be completed
         if (gamepad1.dpad_down || gamepad2.dpad_down){
             Robot.get().emergencyStop();
         }
 
+        //fires
         if (gamepad1.right_bumper) {
             Robot.get().fire();
         }
+
+        gamepad2RightTrigger.toggle(gamepad2.right_trigger>=TRIGGER_DEADZONE);
+        gamepad2LeftTrigger.toggle(gamepad2.left_trigger>=TRIGGER_DEADZONE);
+        if (gamepad2RightTrigger.justChanged()){
+            launcherSpeedFraction += 0.1;
+        } else if (gamepad2LeftTrigger.justChanged()){
+            launcherSpeedFraction -= 0.1;
+        }
+        launcherSpeedFraction = Range.clip(launcherSpeedFraction,0.1,1);
+        Robot.get().setLauncherTargetVelocity(Config.maxLauncherSpeed*launcherSpeedFraction);
+
+        /*============
+        Collector Control
+         ============*/
 
         collecting.toggle(gamepad1.left_bumper);
         if (collecting.getBool()){
@@ -62,6 +83,10 @@ public class TwoControllerTwo extends TeleOpScript {
             Robot.get().stopCollecting();
         }
 
+        /*============
+        Drive Control
+         ============*/
+
         if (moveStraightFast.getBool()) {
             tgtPowerLeft = -gamepad1.left_stick_y;
             tgtPowerRight = -gamepad1.left_stick_y;
@@ -69,11 +94,12 @@ public class TwoControllerTwo extends TeleOpScript {
 
         if (!moveStraightFast.getBool()) {
             // press both bumpers to get full power
-            if (gamepad1.b)
+            if (gamepad1.b) {
                 driveScale = 0.65;
                 // press neither bumper to get quarter power
-            else
+            }else {
                 driveScale = 0.35;
+            }
         }
 
         tgtPowerLeft = driveScale * turn;
@@ -90,16 +116,25 @@ public class TwoControllerTwo extends TeleOpScript {
 //        telemetry.addData("left odo", Robot.get().getLeftOdo());
 //        telemetry.addData("right odo", Robot.get().getRightOdo());
 //        telemetry.addData("mid odo", Robot.get().getMidOdo());
-        telemetry.addData("FastSetting On/Off", moveStraightFast.getBool());
-        telemetry.addData("elapsed time: T+", deltaTime.seconds());
+        telemetry.addLine("========\uD83D\uDD28===========\uD83D\uDD28=====");
+        telemetry.addLine("MR.SWANKHAMMER CONTROL PANEL");
+        telemetry.addLine("============================");
+        telemetry.addData("Mission Time: T+", deltaTime.seconds());
+        telemetry.addData("Loop Time:", loopTime.milliseconds());
 //        telemetry.addData("Rotation(IMU)", Robot.get().getHeading(AngleUnit.DEGREES));
         //telemetry.addData("Go straight on/off", goStraight.getBool());
+
+        telemetry.addLine("====LAUNCHER====");
+        telemetry.addData("Mode", Robot.get().getLaunchMode() );
+        telemetry.addData("Current Velocity", Robot.get().getLauncherVelocity());
+        telemetry.addData("Target Velocity", Config.maxLauncherSpeed*launcherSpeedFraction);
+        telemetry.addLine("====DRIVE====");
+        telemetry.addData("FastSetting On/Off", moveStraightFast.getBool());
+        telemetry.addLine("====COLLECTOR====");
         telemetry.addData("collector on/off", collecting.getBool());
-        telemetry.addData("LaunchMode", Robot.get().getLaunchMode() );
-        telemetry.addData("LaunchVelocity", Robot.get().getLauncherVelocity());
-        telemetry.addData("ElapsedTime:", deltaTime.milliseconds());
+
         //telemetry.addData("LaunchVelocityDirectRef", ((DcMotorEx)DataHub.hardwareMap.get(DcMotor.class, Config.launcherMotor1)).getCurrentPosition() );
-        deltaTime.reset();
+        loopTime.reset();
     }
 
     @Override
@@ -111,11 +146,6 @@ public class TwoControllerTwo extends TeleOpScript {
 
         moveStraightFast = new Toggle(false);
         collecting = new Toggle(false);
-        launcherOff = new Toggle(false);
-        spool = new Toggle(false);
-        continuousFire = new Toggle(false);
-        fire = new Toggle(false);
-        launchOverride = new Toggle(false);
         stopCollecting = new Toggle(false);
 
     }
