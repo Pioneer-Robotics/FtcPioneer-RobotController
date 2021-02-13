122293679c6de1e0fcf57d2c6413834ac120f24c
gianluca-tenino <gianluca.tenino@gmail.com>
2021-02-12 18:06:21 -0700

    Added CompetitionTeleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MatchModes/CompetitionTeleop.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.OpModes.MatchModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Hardware.Robot;
+import org.firstinspires.ftc.teamcode.OpModes.Autos.NullAuto;
+import org.firstinspires.ftc.teamcode.OpModes.TeleOps.ExampleTeleOp;
+import org.firstinspires.ftc.teamcode.OpModes.TeleOps.TwoController.TwoControllerTwo;
+
+@TeleOp (name = "CompTeleOp", group = "example")
+public class CompetitionTeleop extends GenericOpMode {
+    @Override
+    public void selectAutoAndTeleOp(){
+        auto = new NullAuto(); //you need this even if you don't use it
+        teleOp = new TwoControllerTwo();
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
+        selectAutoAndTeleOp();
+        initAndWaitForStart();
+        while(opModeIsActive()) {
+            teleOp.loop();
+            telemetry.update();
+            Robot.get().update(false);
+        }
+    }
+}
