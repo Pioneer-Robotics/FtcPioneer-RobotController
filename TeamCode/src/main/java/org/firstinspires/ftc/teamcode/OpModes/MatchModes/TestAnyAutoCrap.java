01464e361f7397e3be14e77391a1159e5904f677
sethrojas21 <70863997+sethrojas21@users.noreply.github.com>
2021-02-27 13:36:38 -0700

    pushing so joe can pull

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MatchModes/TestAnyAutoCrap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MatchModes/TestAnyAutoCrap.java
@@ -6,6 +6,7 @@ import org.firstinspires.ftc.teamcode.Hardware.Robot;
 import org.firstinspires.ftc.teamcode.OpModes.Autos.DeliverWobbleGoal_ABC.GoToABC;
 
 import org.firstinspires.ftc.teamcode.OpModes.Autos.NullAuto;
+import org.firstinspires.ftc.teamcode.OpModes.Autos.TestDriveStraightPlusCheckRings;
 import org.firstinspires.ftc.teamcode.OpModes.Autos.TestLasors;
 import org.firstinspires.ftc.teamcode.OpModes.TeleOps.TeleOpStandard;
 
@@ -13,7 +14,7 @@ import org.firstinspires.ftc.teamcode.OpModes.TeleOps.TeleOpStandard;
 public class TestAnyAutoCrap extends GenericOpMode{
     @Override
     public void selectAutoAndTeleOp() {
-        auto = new NullAuto();
+        auto = new TestDriveStraightPlusCheckRings();
         teleOp = new TeleOpStandard(); //not sure if this is right teleOp, good enough for now
     }
 
